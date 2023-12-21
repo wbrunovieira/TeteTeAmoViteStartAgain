5569768764c3bf8b44231d14e57f84f9e3ffd06f
wbrunovieira <wbrunovieira77@gmail.com>
2023-12-21 12:41:57 -0300

    added hearts and style.css to the main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import './style.css'
 import * as THREE from 'three';
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
 
 
 
@@ -27,8 +31,10 @@ function render() {
 
 // Carregar a fonte e adicionar o texto
 const loader = new FontLoader();
+const loaderHeart = new GLTFLoader();
+
 loader.load('/fonts/helvetiker_regular.typeface.json', function (font) {
-    const textGeometry = new TextGeometry('Tete eu Te amo !', {
+    const textGeometry = new TextGeometry('Tete, eu Te amo !', {
         font: font,
         size: 5,
         height: 2,
@@ -49,6 +55,43 @@ loader.load('/fonts/helvetiker_regular.typeface.json', function (font) {
     textMesh.position.set(-30, 0, 0); // Ajuste a posição conforme necessário
     scene.add(textMesh);
 });
+
+
+loaderHeart.load('/models/heart.glb', function (gltf) {
+    for (let i = 0; i < 1000; i++) {
+        // Clona o modelo
+        const heart = gltf.scene.clone();
+
+        // Varia a posição aleatoriamente
+        heart.position.set(
+            (Math.random() - 0.5) * 100, // x
+            (Math.random() - 0.5) * 100, // y
+            (Math.random() - 0.5) * 100  // z
+        );
+
+        // Varia a rotação aleatoriamente
+        heart.rotation.set(
+            Math.random() * Math.PI, // x
+            Math.random() * Math.PI, // y
+            Math.random() * Math.PI  // z
+        );
+
+        // Varia o tamanho aleatoriamente
+        const scale = Math.random() * 0.5 + 0.5; // Entre 0.5 e 1.5
+        heart.scale.set(scale, scale, scale);
+
+        // Adiciona o coração na cena
+        scene.add(heart);
+    }
+
+    // Re-renderiza a cena
+    render();
+}, undefined, function (error) {
+    console.error(error);
+});
+
+
+
 
 // Função de animação
 function animate() {
